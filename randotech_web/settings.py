b6173723205ef8427908d9c966b7f2ba212c6a2c
aliyumuhd9990 <aliyumuhammad9990@gmail.com>
2025-07-18 20:03:05 +0300

    deployment 06

--- randotech_web/settings.py
+++ randotech_web/settings.py
@@ -138,10 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR/ 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_DIRES = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+STATIC_ROOT = BASE_DIR / "static"
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/img/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'img/')
