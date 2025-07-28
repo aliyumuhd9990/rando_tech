17afaae0b891921a5d010cee4f38d4b723cc85ba
aliyumuhd9990 <aliyumuhammad9990@gmail.com>
2025-07-28 20:29:13 +0300

    update 03

--- randotech_web/settings.py
+++ randotech_web/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+#This will work
 
 STATIC_URL = 'static/'
 # STATICFILES_DIRES = [
